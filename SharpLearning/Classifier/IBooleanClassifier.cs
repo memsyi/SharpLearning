a6c9c979e619a36d52f821df59d41e41a385167b
Peter Gerrard <32jezza@gmail.com>
2015-01-25 00:16:53 +0000

    Add a basic k nearest neighbour algorithm.

---
+++ SharpLearning/Classifier/IBooleanClassifier.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace SharpLearning.Classifier
+{
+    /// <summary>
+    /// Classifies an item of type <typeparamref name="T"/> as a boolean.
+    /// </summary>
+    public interface IBooleanClassifier<T>
+    {
+        /// <summary>
+        /// Trains the classifier on the given <paramref name="trainingSet"/>.
+        /// </summary>
+        /// <param name="trainingSet">The map from inputs to outputs to train on.</param>
+        void Train(IDictionary<T, bool> trainingSet);
+
+        /// <summary>
+        /// Classify the given <paramref name="item"/> as a boolean.
+        /// </summary>
+        bool Classify(T item);
+    }
+}
---
+++ SharpLearning/Classifier/KNearestNeighbourClassifier.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SharpLearning.Classifier
+{
+    /// <summary>
+    /// Classifies an item using the k nearest neighbour algorithm.
+    /// See http://en.wikipedia.org/wiki/K-nearest_neighbors_algorithm.
+    /// </summary>
+    public class KNearestNeighbourClassifier<T> : IBooleanClassifier<T>
+    {
+        private readonly IDictionary<T, bool> _trainingSet = new Dictionary<T, bool>();
+        private readonly int _k;
+        private readonly Func<T, T, int> _distanceFunction;
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        /// <param name="k">The number of nearest neighbours to consider.</param>
+        /// <param name="distanceFunction">Calculate the distance between two items.</param>
+        public KNearestNeighbourClassifier(int k, Func<T, T, int> distanceFunction)
+        {
+            _k = k;
+            _distanceFunction = distanceFunction;
+        }
+
+        public void Train(IDictionary<T, bool> trainingSet)
+        {
+            foreach (var trainingItem in trainingSet)
+            {
+                _trainingSet.Add(trainingItem);
+            }
+        }
+
+        public bool Classify(T item)
+        {
+            var k = Math.Min(_k, _trainingSet.Count);
+            var kNearest = _trainingSet.OrderBy(kvp => _distanceFunction(kvp.Key, item)).Take(k);
+            var numTrue = kNearest.Select(kvp => kvp.Value).Count(value => value);
+            return 2*numTrue >= k;
+        }
+    }
+}
